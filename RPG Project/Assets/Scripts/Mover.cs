eabf6739cc62281fa61a6255aceff60ecc24a9ce
Yunti <n.pawcio@gmail.com>
2022-12-04 01:28:20 +0100

    Moving

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -19,11 +19,23 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
 
+        UpdateAnimator();
+
+    }
+
+
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMesh.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 
     private void MoveToCursor()
